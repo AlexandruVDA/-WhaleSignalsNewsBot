7234e8210530ae267dff37f3adec407cfe0f284e
AlexandruVDA <80089108+AlexandruVDA@users.noreply.github.com>
2026-06-15 12:55:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ function shortenTitle(text = "") {
 
   if (text.length <= 37) return text;
 
-  return text.slice(0, 34).trim() + "...";
+  return text.slice(0, 32).trim() + "...";
 }
 
 function getDescription(item) {
